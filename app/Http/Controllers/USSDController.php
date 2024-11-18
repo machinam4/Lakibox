9583c37694e4a72ebd01c1440baa15b97f0c3313
Machina <machina@localhost.localdomain>
2024-11-18 11:07:28 +0300

    added multiple steps

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -14,7 +14,7 @@ class USSDController extends Controller
         $message = $data['ussdString'] ?? null;
         $phoneNumber = $data['msisdn'];
         $sessionId = $data['sessionId'];
-        $sms_shortcode = $data['serviceCode'];
+        $sms_shortcode = urldecode($data['serviceCode']) === '*835*564#' ? '24119' : '23089';
         Log::info($data);
 
         if ($message) {
