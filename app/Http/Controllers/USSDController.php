e4fc310473f71c2e0f4fde691438e2d24851a0b1
Machina <machina@localhost.localdomain>
2024-11-18 09:56:21 +0300

    added multiple steps

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
 
 class USSDController extends Controller
 {
