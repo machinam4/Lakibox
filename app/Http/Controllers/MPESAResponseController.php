99691b05b3792f48a3f20dabe268d1314e49815b
Machina <machina@localhost.localdomain>
2024-11-19 12:41:35 +0300

    changed to Onfon Media

--- app/Http/Controllers/MPESAResponseController.php
+++ app/Http/Controllers/MPESAResponseController.php
@@ -73,7 +73,7 @@ class MPESAResponseController extends Controller
             $bet_result = $BETS->placeBet($bet->BillRefNumber);
             // Log::info($bet_result);
             if ($bet_result['status'] == 'win') {
-                $sms = "Hongera!\n**\nUlichagua $bet->BillRefNumber\n**\nBox 1- ".$bet_result['values']['box1']."\nBox 2- ".$bet_result['values']['box2']."\nBox 3- ".$bet_result['values']['box3']."\nBox 4- ".$bet_result['values']['box4']."\nBox 5- ".$bet_result['values']['box5']."\n**\n**\nChagua tena USHINDE.";
+                $sms = "Hongera!\n**\nUlichagua $bet->BillRefNumber\n**\nBox 1- ".$bet_result['values']['box1']."\nBox 2- ".$bet_result['values']['box2']."\nBox 3- ".$bet_result['values']['box3']."\nBox 4- ".$bet_result['values']['box4']."\nBox 5- ".$bet_result['values']['box5']."\n**\n**\nPiga *864# Kujaribu tena.";
                 // send winnings
                 $WINS = new WithdrawalController;
                 $sendWinnings = $WINS->b2cPaymentRequest($bet->MSISDN, $bet_result['amount_won'], $bet->SmsShortcode);
@@ -86,7 +86,7 @@ class MPESAResponseController extends Controller
                 $smssend = $SMS->sendSMS($sms, $bet->MSISDN, $bet->SmsShortcode);
             } else {
                 // Log::info('lost');
-                $sms = "Umepoteza!\n**\nUlichagua $bet->BillRefNumber\n**\nBox 1- ".$bet_result['values']['box1']."\nBox 2- ".$bet_result['values']['box2']."\nBox 3- ".$bet_result['values']['box3']."\nBox 4- ".$bet_result['values']['box4']."\nBox 5- ".$bet_result['values']['box5']."\n**\n**\nChagua tena USHINDE.";
+                $sms = "Umepoteza!\n**\nUlichagua $bet->BillRefNumber\n**\nBox 1- ".$bet_result['values']['box1']."\nBox 2- ".$bet_result['values']['box2']."\nBox 3- ".$bet_result['values']['box3']."\nBox 4- ".$bet_result['values']['box4']."\nBox 5- ".$bet_result['values']['box5']."\n**\n**\nPiga *864# Kujaribu tena.";
                 // $SMS = new LidenController;
                 $SMS = new OnfonSmsController;
                 $smssend = $SMS->sendSMS($sms, $bet->MSISDN, $bet->SmsShortcode);
